24efd1b8e675a6d9256232b0a74d2dbf0fe65879
oussama hdidou <oussamahdidou15@gmail.com>
2023-10-26 12:59:41 +0100

    oninit

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent {
-
+  users = [
+    { name: 'oussama', id: '1' },
+    { name: 'ahmed', id: '2' },
+    { name: 'joedoe', id: '3' },
+    { name: 'unknown', id: '4' },
+  ];
+  /**
+   *
+   */
+  constructor() {}
+  ngOnInit(): void {}
 }
