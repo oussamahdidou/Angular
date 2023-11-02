ded69e8eb57a284ad590e5e7af032274a0fb67ba
oussama hdidou <oussamahdidou15@gmail.com>
2023-11-02 19:31:48 +0100

    services

--- src/app/pokemon-base/pokemon-base.module.ts
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { Items_serviceService } from '../services/items_service.service';
 
 @NgModule({
   declarations: [PokemonListComponent, ListComponent, DetailComponent],
   imports: [CommonModule],
   exports: [PokemonListComponent, ListComponent, DetailComponent],
+  providers: [Items_serviceService],
 })
 export class PokemonBaseModule {}
